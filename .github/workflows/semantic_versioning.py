53769714b56a9818dfbf0c032da5950db4812263
pkj1993 <140062112+pkj1993@users.noreply.github.com>
2023-08-02 06:55:55 +0530

    Update semantic_versioning.py

--- .github/workflows/semantic_versioning.py
+++ .github/workflows/semantic_versioning.py
@@ -1,51 +1,40 @@
-import os
 import subprocess
 import re
 
-def get_current_version():
+def get_latest_tag():
     try:
-        with open('version.txt', 'r') as version_file:
-            version = version_file.read().strip()
-            return version
-    except FileNotFoundError:
-        return '0.1.0'  # Default version if version.txt doesn't exist
+        tags_output = subprocess.check_output(["git", "ls-remote", "--tags", "origin"])
+        tags_list = tags_output.decode("utf-8").strip().split("\n")
+        latest_tag = max(tags_list, key=lambda x: [int(d) for d in re.findall(r'\d+', x)])
+        return latest_tag.strip()
+    except subprocess.CalledProcessError:
+        raise RuntimeError("Failed to get the latest GitHub tag.")
 
-def bump_version(version, part='patch'):
-    major, minor, patch = map(int, version.split('.'))
+def symmetric_increment_version(version):
+    parts = version.split('.')
+    if len(parts) != 3:
+        raise ValueError("Invalid version format. Expected 'major.minor.patch'.")
     
-    if part == 'major':
-        major += 1
-        minor = 0
-        patch = 0
-    elif part == 'minor':
-        minor += 1
-        patch = 0
-    else:
-        patch += 1
-    
-    new_version = f"{major}.{minor}.{patch}"
+    major, minor, patch = map(int, parts)
+    new_version = f"{minor}.{major}.{patch}"
     return new_version
 
-def create_tag_and_release(new_version):
-    # Create a Git tag
-    subprocess.run(['git', 'tag', new_version])
-    
-    # Create a GitHub release using 'hub' command (make sure you have 'hub' installed)
-    release_notes = f'Release {new_version}'
-    subprocess.run(['hub', 'release', 'create', new_version, '-m', release_notes])
+def create_git_tag(version):
+    subprocess.run(["git", "tag", version])
+    subprocess.run(["git", "push", "origin", version])
 
 def main():
-    current_version = get_current_version()
-    print(f'Current version: {current_version}')
-    
-    new_version = bump_version(current_version, 'patch')
-    print(f'New version: {new_version}')
-    
-    # Write the new version back to version.txt
-    with open('version.txt', 'w') as version_file:
-        version_file.write(new_version)
-    
-    create_tag_and_release(new_version)
+    try:
+        latest_tag = get_latest_tag()
+        print(f"Latest GitHub tag: {latest_tag}")
+
+        new_version = symmetric_increment_version(latest_tag)
+        print(f"New symmetric version: {new_version}")
+
+        create_git_tag(new_version)
+        print(f"Git tag '{new_version}' created and pushed.")
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
     main()
